6e829ab70e92c8fec86f7aa259d5762a95da67c4
Abhay2004Kumar <abs220502@gmail.com>
2025-12-30 15:51:45 +0530

    added product controller

---
+++ app/controllers/api/v1/products_controller.rb
@@ -0,0 +1,57 @@
+class Api::V1::ProductsController < ApplicationController
+
+  
+   # GET /products
+      def index
+        products = Product.all
+        render json: products
+      end
+
+      # GET /products/:id
+      def show
+        product = Product.find(params[:id])
+        render json: product
+      end
+
+      # POST /products
+      def create
+        product = Product.new(product_params)
+
+        if product.save
+          render json: product, status: :created
+        else
+          render json: { errors: product.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+
+      # PUT /products/:id
+      def update
+        product = Product.find(params[:id])
+
+        if product.update(product_params)
+          render json: product
+        else
+          render json: { errors: product.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+
+      # DELETE /products/:id
+      def destroy
+        product = Product.find(params[:id])
+        product.destroy
+
+        render json: { message: "Product deleted successfully" }
+      end
+
+      private
+
+      def product_params
+        params.require(:product).permit(
+          :name,
+          :description,
+          :price,
+          :stock
+        )
+      end
+
+end
