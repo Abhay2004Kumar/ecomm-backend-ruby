3ee64f1386a43391f797bd040405317412a3618b
Abhay2004Kumar <abs220502@gmail.com>
2025-12-30 16:27:20 +0530

    created user model

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+  has_one :cart, dependent: :destroy
+  has_many :orders, dependent: :destroy
+
+  validates :email, presence: true, uniqueness: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_12_30_102232) do
+ActiveRecord::Schema[8.1].define(version: 2025_12_30_105336) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -48,6 +48,13 @@ ActiveRecord::Schema[8.1].define(version: 2025_12_30_102232) do
     t.string "name"
     t.decimal "price"
     t.integer "stock"
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "email"
+    t.string "name"
     t.datetime "updated_at", null: false
   end
 
